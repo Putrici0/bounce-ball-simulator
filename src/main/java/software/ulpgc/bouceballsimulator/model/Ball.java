fb5cc4bce169a26c596ad238470dfcc543b8138b
paull <paullopisparrilla@gmail.com>
2025-01-19 13:42:24 +0000

    Model classes completed.

--- src/main/java/software/ulpgc/bouceballsimulator/model/Ball.java
+++ src/main/java/software/ulpgc/bouceballsimulator/model/Ball.java
@@ -1,4 +1,8 @@
 package software.ulpgc.bouceballsimulator.model;
 
-public record Ball() {
+public record Ball(double radius, double restitution) {
+
+    public static Ball create(double radius, double restitution) {
+        return new Ball(radius, restitution);
+    }
 }
--- src/main/java/software/ulpgc/bouceballsimulator/model/BallSimulation.java
+++ src/main/java/software/ulpgc/bouceballsimulator/model/BallSimulation.java
@@ -1,4 +1,39 @@
 package software.ulpgc.bouceballsimulator.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BallSimulation {
+    private final List<Ball> balls = new ArrayList<>();
+    private final List<Double> initialHeights = new ArrayList<>();
+    private final List<Integer> positionsX = new ArrayList<>();
+    private final int groundLevel;
+    private final int distanceBetweenBalls;
+
+    public BallSimulation(int groundLevel, int distanceBetweenBalls) {
+        this.groundLevel = groundLevel;
+        this.distanceBetweenBalls = distanceBetweenBalls;
+    }
+
+    public void addBall(Ball ball, double initialHeight) {
+        balls.add(ball);
+        initialHeights.add(initialHeight);
+        positionsX.add(100 + (balls.size() - 1) * distanceBetweenBalls);
+    }
+
+    public List<Ball> getBalls() {
+        return balls;
+    }
+
+    public List<Double> getInitialHeights() {
+        return initialHeights;
+    }
+
+    public List<Integer> getPositionsX() {
+        return positionsX;
+    }
+
+    public int getGroundLevel() {
+        return groundLevel;
+    }
 }
